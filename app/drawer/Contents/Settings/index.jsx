f1c625f863bd2ad8fd24dc59a060edb692ac80c2
Anthony Siu <Siu_Anthony@cat.com>
2018-12-27 17:19:23 -0600

    wip before pose in Drawer

--- app/drawer/Contents/Settings/index.jsx
+++ app/drawer/Contents/Settings/index.jsx
@@ -2,34 +2,86 @@ import constants from './../../../constants.js';
 import './app.css';
 
 import React, { Component } from 'react';
+import posed, { PoseGroup } from 'react-pose'
 
+const positionType = function(){};
+positionType.Top = -1;
+positionType.Center = 0;
+positionType.Bottom = 1;
+
+const poseHeadingConfig = {
+  top: { opacity: 0.0,
+             // y: "-100%",
+             transition: { duration: 1000 } },
+  center : { opacity: 1,
+             // y: "0%",
+            transition: { duration: 1000 } },
+  initialPose: 'bottom'
+}
+
+const BoxHeading = posed.div(poseHeadingConfig)
+
+const poseZConfig = {
+  top: { "z-index": 0.0,
+             // y: "-100%",
+             transition: { duration: 1000 } },
+  center : { "z-index": 1,
+             // y: "0%",
+            transition: { duration: 1000 } },
+  initialPose: 'bottom'
+}
+
+const BoxZ = posed.div(poseZConfig)
+
+var main = null;
+var header = null;
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // position: positionType.Center
+      position: positionType.Center
     }
   }
 
   componentDidMount(){
-    // main = document.getElementById("main-content")
-    // main.addEventListener("scroll", this._calcScroll.bind(this, main))
-    // header = document.getElementById("SettingsBar")
+    header = document.getElementById("SettingsBar")
+    main = document.getElementById("main-content")
+    main.addEventListener("scroll", this._calcScroll.bind(this, main))
   }
   componentWillUnmount() {
-    // main.removeEventListener('scroll', this._calcScroll)
+    main.removeEventListener('scroll', this._calcScroll)
   }
-  
+  _calcScroll() {
+      var scrollPos = main.scrollTop;
+      // console.log('header.height ' + header.offsetHeight)
+      if (scrollPos > header.offsetHeight/2) {
+          // console.log('scroll past' + scrollPos)
+          this.setState({
+              position: positionType.Top
+          });
+      } else {
+          // console.log('scroll back' + scrollPos)
+          this.setState({
+              position: positionType.Center
+          });
+      }
+  }
   render() {
     let positionState = this.state.position;
     // console.log(positionState)
     return (
       <div className='SettingsContainer' id="Settings">
-          <header className="" id="SettingsBar" >
+
+          <BoxHeading className="" id=""
+                          pose={positionState == positionType.Bottom ? 'bottom' :
+                                positionState == positionType.Center ? 'center' : 'top'}>
+                    <header className="" id="SettingsBar" >
                       <span className="" id="SettingsName">Bob</span>
                       <span className="" id="SettingsName">bob@email.com</span>
-          </header>
+                    </header>
+          </BoxHeading>
+
 
         <h2>Content</h2>
         <h2>Content</h2>
